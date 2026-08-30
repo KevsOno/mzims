87061acec874f561052c9f47528cb7c96b08169b
KevsOno <mickevsono@gmail.com>
2026-08-30 23:05:39 +0100

    Remove unused import for Product in __init__.py

--- api/app/models/__init__.py
+++ api/app/models/__init__.py
@@ -1,5 +1,1 @@
-from .product import Product, ProductCreate, ProductUpdate, ProductResponse
-from .order import Order, OrderCreate, OrderResponse, OrderItem, OrderItemCreate
-from .customer import Customer, CustomerCreate
-from .address import Address, AddressCreate
-from .fragrance_request import FragranceRequest, FragranceRequestCreate
+from .product import ProductCreate, ProductUpdate, ProductResponse
