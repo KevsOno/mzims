705918e961d3247ea48e774f891528c672c86ca6
KevsOno <mickevsono@gmail.com>
2026-08-30 16:53:11 +0100

    Initialize App component with routing setup

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import PaymentVerify from './pages/PaymentVerify';
+import FragranceRequest from './pages/FragranceRequest';
+import { CartProvider } from './store/CartContext';
+import { AuthProvider } from './store/AuthContext';
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+        <CartProvider>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/product/:slug" element={<ProductDetail />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/payment/verify" element={<PaymentVerify />} />
+              <Route path="/request-scent" element={<FragranceRequest />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </Layout>
+        </CartProvider>
+      </AuthProvider>
+    </Router>
+  );
+}
+
+export default App;
