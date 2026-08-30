43962ad3060f8f5842bd4b96dbf0a03f723a43db
KevsOno <mickevsono@gmail.com>
2026-08-30 13:05:52 +0100

    Initialize models by importing necessary classes

---
+++ api/app/models/__init__.py
@@ -0,0 +1,5 @@
+from .product import Product, ProductCreate, ProductUpdate, ProductResponse
+from .order import Order, OrderCreate, OrderResponse, OrderItem, OrderItemCreate
+from .customer import Customer, CustomerCreate
+from .address import Address, AddressCreate
+from .fragrance_request import FragranceRequest, FragranceRequestCreate
