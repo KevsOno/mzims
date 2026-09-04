244b49705812b1d0b17a662cb572ca24ca98c7c6
KevsOno <mickevsono@gmail.com>
2026-09-04 23:02:59 +0100

    Add model imports in __init__.py
    Added imports for Order, OrderItem, Customer, and Address models.

--- api/app/models/__init__.py
+++ api/app/models/__init__.py
@@ -1,3 +1,14 @@
+# api/app/models/__init__.py
 
-# Also export any other models you have
+# Existing imports
 from .product import ProductCreate, ProductUpdate, ProductResponse
+
+# Add these imports - adjust file names as needed
+from .order import Order, OrderItem          # if defined in order.py
+from .customer import Customer              # if in customer.py
+from .address import Address                # if in address.py
+
+# If you have many models, you can also export everything from each file:
+# from .order import *
+# from .customer import *
+# from .address import *
