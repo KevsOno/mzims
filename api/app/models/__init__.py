c9dac89b1a04f5cd0198c5adcfd12de40feea7de
KevsOno <mickevsono@gmail.com>
2026-09-04 22:56:59 +0100

    Update __init__.py

--- api/app/models/__init__.py
+++ api/app/models/__init__.py
@@ -1,1 +1,9 @@
+# api/app/models/__init__.py
+
+from .order import Order
+from .order_item import OrderItem   # if this file exists
+from .customer import Customer
+from .address import Address
+
+# Also export any other models you have
 from .product import ProductCreate, ProductUpdate, ProductResponse
